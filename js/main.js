58942b60fd5670ee96663f09c81498782a502d8d
mostafa elbanna <mostafashabanelbanna@gmail.com>
2020-04-15 15:40:50 +0200

    right to left direction

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ function toggleNav() {
   var Sidenav = document.getElementById("mySidenav");
   if (Sidenav.style.width === "250px") {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("main").style.marginRight = "0";
   } else {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main").style.marginRight = "250px";
   }
 }
